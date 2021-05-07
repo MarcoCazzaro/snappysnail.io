ec90f74837f2d8cfee92d0fdcece98fa64fe9a9e
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2021-05-07 17:26:47 +0200

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        @include('partials.preloads')
         @include('partials.seo')
 
         <!-- Fonts -->
---
+++ resources/views/partials/preloads.blade.php
@@ -0,0 +1,16 @@
+<link rel="preload" href="{{ asset('img/snappysnail-logo.png') }}" as="image">
+<link rel="preload" href="{{ url(mix('css/app.css')) }}" as="style">
+<link rel="preload" href="{{ url(mix('js/app.js')) }}" as="script">
+<link
+    rel="preload"
+    href="https://fonts.googleapis.com/css?family=Quicksand&display=swap"
+    as="style"
+    onload="this.onload=null;this.rel='stylesheet'"
+/>
+<noscript>
+    <link
+        href="https://fonts.googleapis.com/css?family=Quicksand&display=swap"
+        rel="stylesheet"
+        type="text/css"
+    />
+</noscript>
