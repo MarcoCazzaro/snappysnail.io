90d7e15bdf4e74ebb148360557f4d6268a5db2ab
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2023-01-11 18:03:18 +0100

    Camping europa and indeeper

---
+++ resources/views/partials/works/camping-europa.blade.php
@@ -0,0 +1,14 @@
+<div class="snail-work camping-europa">
+    <div class="max-w-md mx-auto bg-white dark:bg-gray-800 rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="md:flex-shrink-0">
+                <img class="h-48 w-full object-cover md:h-full md:w-48" src="{{ asset('img/works/camping-europa.jpg') }}" alt="campingeuropa.com WordPress theme">
+            </div>
+            <div class="p-8 text-justify">
+                <div class="tracking-wide text-sm font-semibold snail-fg-color-brand">Works</div>
+                <a href="https://www.campingeuropa.com" class="block mt-1 text-lg leading-tight font-medium text-black dark:text-gray-400 hover:underline" target="_blank">Campingeuropa.com WordPress theme</a>
+                <p class="mt-2 text-gray-500">In 2022, in collaboration with Hyled S.r.l., I created campingeuropa.com WordPress theme, starting from <a href="https://underscores.me/" target="_blank" rel="nofollow">Underscores</a>, and adding <a href="https://getbootstrap.com/" target="_blank" rel="nofollow">Bootstrap</a>.</p>
+            </div>
+        </div>
+    </div>
+</div>
