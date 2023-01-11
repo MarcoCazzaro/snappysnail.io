ecc718e7c939d20d8e48539e18280f3d8ceed7ab
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2023-01-11 16:48:30 +0100

    Tmfwitw

---
+++ resources/views/partials/works/tmfwitw.blade.php
@@ -0,0 +1,14 @@
+<div class="snail-work tmfwitw">
+	<div class="max-w-md mx-auto bg-white dark:bg-gray-800 rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
+		<div class="md:flex">
+			<div class="md:flex-shrink-0">
+				<img class="h-48 w-full object-cover md:h-full md:w-48" src="{{ asset('img/works/tmfwitw.jpg') }}" alt="The Most Famous Website In The World">
+			</div>
+			<div class="p-8 text-justify">
+				<div class="tracking-wide text-sm font-semibold snail-fg-color-brand">Works</div>
+				<a href="https://themostfamouswebsiteintheworld.com" class="block mt-1 text-lg leading-tight font-medium text-black dark:text-gray-400 hover:underline" target="_blank">The Most Famous Website In The World</a>
+				<p class="mt-2 text-gray-500">I created The Most Famous Website In The World with <a href="https://devdojo.com/thinkverse/getting-started-with-tall-stack" target="_blank">TALL stack</a>.</p>
+			</div>
+		</div>
+	</div>
+</div>
