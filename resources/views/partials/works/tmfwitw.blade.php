198d8fc4f88d2ec0d2f944cc20748334005d383d
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2023-03-08 14:44:48 +0100

    MadeWithLaravel

--- resources/views/partials/works/tmfwitw.blade.php
+++ resources/views/partials/works/tmfwitw.blade.php
@@ -8,6 +8,7 @@
 				<div class="tracking-wide text-sm font-semibold snail-fg-color-brand">Works</div>
 				<a href="https://themostfamouswebsiteintheworld.com" class="block mt-1 text-lg leading-tight font-medium text-black dark:text-gray-400 hover:underline" target="_blank">The Most Famous Website In The World</a>
 				<p class="mt-2 text-gray-500">I created The Most Famous Website In The World with <a href="https://devdojo.com/thinkverse/getting-started-with-tall-stack" target="_blank">TALL stack</a>.</p>
+                <p>The project has also been listed on <a target="_blank" href="https://madewithlaravel.com/the-most-famous-website-in-the-world">Made With Laravel</a>!</p>
 			</div>
 		</div>
 	</div>
