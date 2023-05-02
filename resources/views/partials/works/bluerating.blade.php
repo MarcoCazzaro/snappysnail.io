09190b1b37a1be8e416b5415e8018078d2383d90
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2023-05-02 16:19:05 +0200

    .

--- resources/views/partials/works/bluerating.blade.php
+++ resources/views/partials/works/bluerating.blade.php
@@ -8,7 +8,6 @@
 				<div class="tracking-wide text-sm font-semibold snail-fg-color-brand">Works</div>
 				<a href="https://www.bluerating.com/?who-made-this-website=snappy" class="block mt-1 text-lg leading-tight font-medium text-black dark:text-gray-400 hover:underline" target="_blank">Bluerating.com WordPress theme</a>
 				<p class="mt-2 text-gray-500">In 2023 I created Bluerating.com WordPress theme with <a href="https://getbootstrap.com/docs/5.2" target="_blank" rel="nofollow">Bootstrap 5</a>, based on a graphic study created by <a href="https://lucaferroni.com/" target="_blank" rel="nofollow">Luca Ferroni</a>.</p>
-				<p class="mt-2 text-gray-500">Bluerating is the first website in Italy devoted entirely to asset management, founded in 1997. Its contents include more than 15,000 financial products, updated daily. The target of reference is composed of financial promoters, investment counselors, consultants, private bankers, banks, asset management firms and individual investors.</p>
 			</div>
 		</div>
 	</div>
