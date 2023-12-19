a6f1b032456e74ef23873221ed2ee2a14e7dc55b
Marco Cazzaro <info@snappysnail.io>
2023-12-19 12:21:07 +0100

    Forbes 2023

---
+++ resources/views/partials/works/forbes-2023.blade.php
@@ -0,0 +1,14 @@
+<div class="snail-work forbes-2023">
+	<div class="max-w-md mx-auto bg-white dark:bg-zinc-900 rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
+		<div class="md:flex">
+			<div class="md:flex-shrink-0">
+				<img class="h-48 w-full object-cover md:h-full md:w-48" src="{{ asset('img/works/forbes.jpg') }}" alt="Forbes.it WordPress theme">
+			</div>
+			<div class="p-8 text-justify">
+				<div class="tracking-wide text-sm font-semibold snail-fg-color-brand">Works</div>
+				<a href="https://forbes.it/" class="block mt-1 text-lg leading-tight font-medium text-black dark:text-gray-300 hover:underline" target="_blank">Forbes.it 2023 WordPress theme</a>
+				<p class="mt-2 text-gray-500">Based on a design by Men At Code, I created the new Forbes.it WordPress theme 2023, based on Bootstrap 5.3.</p>
+			</div>
+		</div>
+	</div>
+</div>
