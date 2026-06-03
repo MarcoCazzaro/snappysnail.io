811d75b04464f4cb0ed37b295b2216c568c26713
MarcoCazzaro <marco.cazzaro.0@gmail.com>
2026-06-03 08:48:58 +0200

    Footer Linkedin

--- resources/views/components/footer-copyright-line.blade.php
+++ resources/views/components/footer-copyright-line.blade.php
@@ -5,7 +5,7 @@
     <p class="{{ $element_bg }}">Via Monte Grappa 119, San Martino di Lupari (PD) Italy</p>
     <p class="{{ $element_bg }}">P.IVA 03919560130</p>
     <div class="{{ $element_bg }} flex gap-4 items-center justify-center">
-        <a aria-label="Linkedin profile page of Marco Cazzaro" href="https://www.linkedin.com/in/marco-cazzaro-2b6373100/" class="{{ $links_classes }}" target="_blank"><i class="fab fa-linkedin"></i></a>
+        <a aria-label="Linkedin profile page of Marco Cazzaro" href="https://www.linkedin.com/company/snappysnail-di-marco-cazzaro" class="{{ $links_classes }}" target="_blank"><i class="fab fa-linkedin"></i></a>
         <a aria-label="GitHub profile page of Marco Cazzaro" href="https://github.com/MarcoCazzaro" class="{{ $links_classes }}" target="_blank"><i class="fab fa-github"></i></a>
     </div>
     <a class="{{ $element_bg }} {{ $links_classes }}" href="{{ url('/dear-googlebot') }}">Dear GoogleBot <i class="fas fa-robot"></i></a>
